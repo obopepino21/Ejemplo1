78e7c2a1827c8e05a6038471e104a69a3d24fb5e
Andy Usina <andy_uiop@hotmail.com>
2024-05-24 13:57:29 -0500

    actualizacion

--- TableroAjedrez  - Parcial/Tableroscript.js
+++ TableroAjedrez  - Parcial/Tableroscript.js
@@ -1,24 +1,32 @@
 generar = () => {
+  const filaPosicion = parseInt(document.getElementById("fila").value) - 1;
+  const columnaPosicion = parseInt(document.getElementById("columna").value) - 1;
   const tablero = document.getElementById("tablero");
+  
+  tablero.innerHTML = "";
 
   for (let fila = 0; fila < 5; fila++) {
-    const filaElemento = document.createElement("tr");
-    for (let columna = 0; columna < 5; columna++) {
-      const casilla = document.createElement("td");
-      filaElemento.appendChild(casilla);
-      if ((fila + columna) % 2 === 0) {
-        casilla.classList.add("casilla-blanca");
-      } else {
-        casilla.classList.add("casilla-negra");
+      const filaElemento = document.createElement("tr");
+      for (let columna = 0; columna < 5; columna++) {
+          const casilla = document.createElement("td");
+          if ((fila + columna) % 2 === 0) {
+              casilla.classList.add("casilla-blanca");
+          } else {
+              casilla.classList.add("casilla-negra");
+          }
+          
+          if (fila === filaPosicion && columna === columnaPosicion) {
+              const imagen = document.createElement("img");
+              imagen.src = "reina.png";
+              imagen.classList.add("EstiloImagen");
+              casilla.appendChild(imagen);
+          }
+          if (fila === filaPosicion || columna === columnaPosicion || fila + columna === filaPosicion + columnaPosicion || fila - columna === filaPosicion - columnaPosicion){
+            casilla.classList.add("posicion-valida");
+          }
+      
+          filaElemento.appendChild(casilla);
       }
-      // Creación de elemento para generar imagen
-      const Cimagen = document.createElement("img");
-      if (fila === 1 || fila === 3) {
-        Cimagen.src = "Peon.png";
-      }
-      Cimagen.classList.add("EstiloImagen");
-      casilla.appendChild(Cimagen);
-    }
-    tablero.appendChild(filaElemento);
+      tablero.appendChild(filaElemento);
   }
-};
+}
